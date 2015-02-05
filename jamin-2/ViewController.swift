81d20e462266fc052be0ed603e82e5978c3deafc
Max Beizer <max.beizer@gmail.com>
2015-02-04 19:36:26 -0600

    Initial Commit

---
+++ jamin-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jamin-2
+//
+//  Created by Max Beizer on 2/4/15.
+//  Copyright (c) 2015 Max Beizer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
